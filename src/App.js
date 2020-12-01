3b861e0071cc0ab9cce62838ea218fae3cc9386c
Nidilko Slava <55159182+Murmaid-ns@users.noreply.github.com>
2020-12-01 15:09:57 +0200

    animated background DONE

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Header from "./components/header/header.component";
 import AboutMe from "./components/about-me/about-me.component";
 import Projects from "./components/projects/projects.componet";
 import ContactInfo from "./components/contact-info/contact-info.component";
+import AnimatedBackground from "./components/animated-background/animated-background.component";
 
 
 function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <AboutMe/>
-            <Projects/>
-            <ContactInfo/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <AnimatedBackground/>
+      <Header/>
+      <AboutMe/>
+      <Projects/>
+      <ContactInfo/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,68 +4,7 @@ import MyPhoto from './photo.jpeg'
 import Particles from "react-tsparticles";
 
 const Header = () => {
-   const styles = {
-       position: "fixed",
-   }
     return (
-        <div>
-            <Particles style={styles}
-                       id="tsparticles"
-                       options={{
-                           background: {
-                               
-                               color: "#000000"
-                           },
-                           detectRetina: true,
-                           fpsLimit: 60,
-                           interactivity: {
-                               detectsOn: "canvas",
-                               events: {
-                                   resize: true
-                               }
-                           },
-                           particles: {
-                               color: {
-                                   value: "#fff"
-                               },
-                               number: {
-                                   density: {
-                                       enable: true,
-                                       area: 1080
-                                   },
-                                   limit: 0,
-                                   value: 400
-                               },
-                               opacity: {
-                                   animation: {
-                                       enable: true,
-                                       minimumValue: 0.1,
-                                       speed: 0.25,
-                                       sync: false
-                                   },
-                                   random: {
-                                       enable: true,
-                                       minimumValue: 0.1
-                                   },
-                                   value: 1
-                               },
-                               move:{
-                                   enable: true,
-                               },
-                               shape: {
-                                   type: "circle"
-                               },
-
-                               size: {
-                                   random: {
-                                       enable: true,
-                                       minimumValue: 0.5
-                                   },
-                                   value: 3
-                               }
-                           }
-                       }}
-            />
             <header>
                 <div className='ball' style={{backgroundImage: `url(${MyPhoto})`}}/>
                 <div className='name-surname'>
@@ -73,7 +12,7 @@ const Header = () => {
                     <h2>Junior Front-end developer</h2>
                 </div>
             </header>
-        </div>
+
     )
 }
 export default Header;
